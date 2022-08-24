c307d41fe0d030c2c8c78c40a757195a7216a82e
marcoantoniossilva <marcoantoniosilva.tec@gmail.com>
2022-08-23 22:22:19 -0300

    Adicionado métodos de interação com elementos

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -28,4 +28,28 @@ describe("Cypress basics", () => {
     //TODO imprimir log no console
     //TODO escrever o log em um campo de texto
   });
+
+  it("Should find and interect with an element", ()=>{
+    const url = "https://wcaquino.me/cypress/componentes.html";
+
+    // cy = Variável grobal do cypress
+    // Visitando uma url
+    cy.visit(url);
+
+    //Pegando um elemento e verificando o seu 'value'
+    cy.get('#buttonSimple').should('have.value',"Clique Me!");
+
+    //Pegando um elemento e clicando nele
+    //cy.get('#buttonSimple').click();
+
+    //Pegando um elemento
+    //verificando o seu 'value' é igual a 'Clique Me!'
+    //clicando nele
+    //verificando se o seu 'value' é igual a 'Obrigado!'
+    cy.get('#buttonSimple')
+      .should('have.value',"Clique Me!")
+      .click()
+      .should('have.value',"Obrigado!");
+
+  })
 });
